ccd85277d282a151f8e866d32553867a16c2daba
Naoya Maruyama <nmaruyama@nvidia.com>
2020-10-20 19:09:30 -0700

    Add a test for DisjointSet

--- test/cpp/jit/test_gpu.cpp
+++ test/cpp/jit/test_gpu.cpp
@@ -18,6 +18,7 @@
 #include <torch/csrc/jit/codegen/cuda/scheduler.h>
 #include <torch/csrc/jit/codegen/cuda/transform_replay.h>
 #include <torch/csrc/jit/codegen/cuda/transform_rfactor.h>
+#include <torch/csrc/jit/codegen/cuda/disjoint_set.h>
 
 // fuser and IR parser
 #include <torch/csrc/jit/codegen/cuda/parser.h>
@@ -27,6 +28,7 @@
 #include <c10/cuda/CUDAStream.h>
 
 #include <iostream>
+#include <algorithm>
 
 // Tests go in torch::jit
 namespace torch {
@@ -7605,6 +7607,96 @@ TEST(NVFuserTest, FusionGroupGuardRelaxedCheck_CUDA) {
   // passing with dynamic shape
   auto t1 = t0.slice(1, 0, 16, 2);
   TORCH_CHECK(complyWith(t1, tensor_type));
+}
+
+TEST(NVFuserTest, FusionDisjointSet_CUDA) {
+  DisjointSet<int> set;
+
+  const std::set<int> group_x({0, 1, 2});
+  const std::set<int> group_y({3, 4, 5});
+  const std::set<int> group_z({6, 7, 8});
+  const std::vector<std::set<int>> groups({group_x, group_y, group_z});
+  std::set<int> group_all;
+  std::for_each(groups.begin(), groups.end(),
+                [&](const auto& g) {
+                  group_all.insert(g.begin(), g.end());
+                });
+
+  // Initially, nothing should be considered equivalent
+  for (auto i: group_all) {
+    for (auto j: group_all) {
+      TORCH_CHECK(!set.areEquivalent(i, j));
+    }
+  }
+
+  // Sets values in group_x are equivalent
+  for (auto i: group_x) {
+    for (auto j: group_x) {
+      set.join(i, j);
+    }
+  }
+
+  // All values in group_x shoudl be equivalent with each other
+  for (auto i: group_x) {
+    for (auto j: group_x) {
+      TORCH_CHECK(set.areEquivalent(i, j));
+    }
+  }
+  // But nothing else should be equivalent
+  for (auto i: group_all) {
+    for (auto j: group_y) {
+      TORCH_CHECK(!set.areEquivalent(i, j));
+    }
+    for (auto j: group_z) {
+      TORCH_CHECK(!set.areEquivalent(i, j));
+    }
+  }
+
+  // Sets values in group_y are equivalent
+  for (auto i: group_y) {
+    for (auto j: group_y) {
+      set.join(i, j);
+    }
+  }
+
+  // group_x should be still equivalent
+  for (auto i: group_x) {
+    for (auto j: group_x) {
+      TORCH_CHECK(set.areEquivalent(i, j));
+    }
+  }
+  // group_y should be now equivalent
+  for (auto i: group_y) {
+    for (auto j: group_y) {
+      TORCH_CHECK(set.areEquivalent(i, j));
+    }
+  }
+  // But group_z should not be equivalent with anything yet
+  for (auto i: group_all) {
+    for (auto j: group_z) {
+      TORCH_CHECK(!set.areEquivalent(i, j));
+    }
+  }
+
+  // Sets values in group_z are equivalent
+  for (auto i: group_z) {
+    for (auto j: group_z) {
+      set.join(i, j);
+    }
+  }
+
+  // Now each of the three groups should be equivalent within each
+  // group
+  for (size_t gi = 0; gi < groups.size(); ++gi) {
+    for (size_t gj = 0; gj < groups.size(); ++gj) {
+      for (auto i: groups[gi]) {
+        for (auto j: groups[gj]) {
+          TORCH_CHECK((gi == gj && set.areEquivalent(i, j)) ||
+                      (gi != gj && !set.areEquivalent(i, j)));
+        }
+      }
+    }
+  }
 }
 
 } // namespace jit
--- torch/csrc/jit/codegen/cuda/disjoint_set.h
+++ torch/csrc/jit/codegen/cuda/disjoint_set.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <c10/util/Exception.h>
+
 #include <unordered_map>
+#include <vector>
 
 namespace torch {
 namespace jit {
@@ -32,15 +35,15 @@ class DisjointSet {
     // cases where either of the quiv class doesn't exist
     if (!entry_map.count(a) && !entry_map.count(b)) {
       createPoint(a);
-      entry_map[b] = fixedPoint(a);
+      entry_map[b] = getOrCreateFixedPoint(a);
     } else if (!entry_map.count(a)) {
-      entry_map[a] = fixedPoint(b);
+      entry_map[a] = getOrCreateFixedPoint(b);
     } else if (!entry_map.count(b)) {
-      entry_map[b] = fixedPoint(a);
+      entry_map[b] = getOrCreateFixedPoint(a);
     } else {
       // case where both equiv classes exist and need to join
-      const int i0 = fixedPoint(a);
-      const int i1 = fixedPoint(b);
+      const int i0 = getOrCreateFixedPoint(a);
+      const int i1 = getOrCreateFixedPoint(b);
       int new_parent = 0;
       int new_child = 0;
 
@@ -66,13 +69,13 @@ class DisjointSet {
     if (!entry_map.count(a) || !entry_map.count(b)) {
       return false;
     }
-    return fixedPoint(a) == fixedPoint(b);
+    return getOrCreateFixedPoint(a) == getOrCreateFixedPoint(b);
   }
 
  private:
   // Internal fixed point implementation:
   //  Returns the equivalent class that e belongs to
-  int fixedPoint(int e) const {
+  int getFixedPoint(int e) const {
     TORCH_INTERNAL_ASSERT(static_cast<int>(set_map.size()) > e);
     while (set_map[e] != e) {
       // Chasing to fixed point
@@ -87,12 +90,12 @@ class DisjointSet {
   //! \param e element e to find the equiv class for
   //! \returns the equivalent class that e belongs to
   //!
-  int fixedPoint(T e) const {
+  int getOrCreateFixedPoint(T e) const {
     // Handles case when i doesn't have an equivalence class
     TORCH_INTERNAL_ASSERT(entry_map.count(e));
 
     // Use fixed point as a representation for the equiv class
-    return fixedPoint(entry_map.at(e));
+    return getFixedPoint(entry_map.at(e));
   }
 
   //! Utility to create a new equiv class for i
