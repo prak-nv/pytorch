d84b97234d8524a00f88069e7b70a3caf6236fa9
Kevin Stephano <kevin.stephano@gmail.com>
2020-11-04 18:43:39 +0000

    Add Nolint to fusion_id_counter_ static data member as clang-tidy sees it as a non-const global variable.

--- torch/csrc/jit/codegen/cuda/executor.cpp
+++ torch/csrc/jit/codegen/cuda/executor.cpp
@@ -23,7 +23,7 @@ namespace jit {
 namespace fuser {
 namespace cuda {
 
-int FusionExecutor::fusion_id_counter_ = 0;
+int FusionExecutor::fusion_id_counter_ = 0; // NOLINT
 
 std::string FusionExecutor::getStructuredCode(const std::string& kernel) {
   // generating cuda code;
