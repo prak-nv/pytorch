f290ed4cb5b4bb02cfd4d1049733ba8621819ac9
Christian Sarofeen <csarofeen@nvidia.com>
2020-12-25 08:30:50 -0500

    Move expression sorting out of lower loops.

--- torch/csrc/jit/codegen/cuda/lower2device.cpp
+++ torch/csrc/jit/codegen/cuda/lower2device.cpp
@@ -3,6 +3,7 @@
 #include <torch/csrc/jit/codegen/cuda/instrumentation.h>
 #include <torch/csrc/jit/codegen/cuda/ir_iostream.h>
 #include <torch/csrc/jit/codegen/cuda/lower_alias_memory.h>
+#include <torch/csrc/jit/codegen/cuda/lower_expr_sort.h>
 #include <torch/csrc/jit/codegen/cuda/lower_index.h>
 #include <torch/csrc/jit/codegen/cuda/lower_insert_syncs.h>
 #include <torch/csrc/jit/codegen/cuda/lower_loops.h>
@@ -118,8 +119,8 @@ void GpuLower::lower() {
   }
 
   // Run our passes keeping the lowered expressions and forwarding them
-  const auto lowered_exprs =
-      LoopNestGenerator::loweredExprs(fusion_, fusion_->exprs());
+  const auto lowered_exprs = LoopNestGenerator::loweredExprs(
+      fusion_, reorderExprsForComputeAt(fusion_->exprs()));
 
   const auto unrolled_loops =
       UnrollPass::runPass(fusion_, lowered_exprs, preds, ca_root_map);
